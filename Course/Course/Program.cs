1b99b6b3ceaf9c427cbef40b14af3c42d348f927
Iury Chagas <iurychagas@icloud.com>
2020-01-20 15:45:15 -0300

    add saida de dados - em C#

---
+++ Course/Course/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+           var name = "Sicrano Fuliveira";
+           var saldo = 17.321547;
+           var age = 31;
+           
+            Console.WriteLine("Nome: "+ name);
+            Console.WriteLine("Saldo: $"+ saldo.ToString("f3", CultureInfo.InvariantCulture));
+            Console.WriteLine(name +" tem "+ age +" anos, seu saldo atual é de R$"+ saldo.ToString("f3", CultureInfo.InvariantCulture)+ " reais");
+
+            // Placeholder!
+            Console.WriteLine("{0} tem {1} anos, seu saldo atual é de R${2:F2} reais", name, age, saldo);
+
+            // Interpolação!
+            Console.WriteLine($"{name} tem {age} anos, seu saldo atual é de R${saldo:F2} reais");
+            Console.WriteLine($"{name} tem {age} anos, seu saldo atual é de R${saldo.ToString("F3", CultureInfo.InvariantCulture)} reais");
+        }
+    }
+}
