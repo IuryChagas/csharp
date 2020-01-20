470477c6c230c53691a0fca23c1d3d4dc955a4a1
Iury Chagas <iurychagas@icloud.com>
2020-01-20 16:03:47 -0300

    add Exercício de fixação

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,20 +5,30 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-           var name = "Sicrano Fuliveira";
-           var saldo = 17.321547;
-           var age = 31;
-           
-            Console.WriteLine("Nome: "+ name);
-            Console.WriteLine("Saldo: $"+ saldo.ToString("f3", CultureInfo.InvariantCulture));
-            Console.WriteLine(name +" tem "+ age +" anos, seu saldo atual é de R$"+ saldo.ToString("f3", CultureInfo.InvariantCulture)+ " reais");
+            String product1 = "Computador";
+            String product2 = "Mesa de escritório";
 
-            // Placeholder!
-            Console.WriteLine("{0} tem {1} anos, seu saldo atual é de R${2:F2} reais", name, age, saldo);
+            byte age = 30;
+            int code = 5290;
+            char gender = 'M';
 
-            // Interpolação!
-            Console.WriteLine($"{name} tem {age} anos, seu saldo atual é de R${saldo:F2} reais");
-            Console.WriteLine($"{name} tem {age} anos, seu saldo atual é de R${saldo.ToString("F3", CultureInfo.InvariantCulture)} reais");
+            double price1 = 2100.00;
+            double price2 = 650.50;
+            double measure = 53.234567;
+
+            Console.WriteLine("Produtos:");
+
+            Console.WriteLine($"{product1}, cujo preço é $ {price1:F2}");
+            Console.WriteLine($"{product2}, cujo preço é $ {price2:F2}");
+            
+            Console.WriteLine();
+
+            Console.WriteLine($"Registro: {age} anos de idade, código {code} e gênero: {gender}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Medida com oito casas decimais: {measure:F8}");
+            Console.WriteLine($"Arredondando (três casas decimais): {measure.ToString("F3", CultureInfo.InvariantCulture)}");
         }
     }
 }
