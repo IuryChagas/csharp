d3014735f2c89234835ab06f471f8a6e2374933b
Iury Chagas <iurychagas@icloud.com>
2020-01-20 16:39:56 -0300

    add Entrada de Dados intro

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,30 +5,29 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            String product1 = "Computador";
-            String product2 = "Mesa de escritório";
 
-            byte age = 30;
-            int code = 5290;
-            char gender = 'M';
+            Console.Write("Digite: ");
+            String frase = Console.ReadLine();
 
-            double price1 = 2100.00;
-            double price2 = 650.50;
-            double measure = 53.234567;
+            String x = Console.ReadLine();
+            String y = Console.ReadLine();
+            String z = Console.ReadLine();
 
-            Console.WriteLine("Produtos:");
+            String[] vet = Console.ReadLine().Split(' ');
+            String a = vet[0];
+            String b = vet[1];
+            String c = vet[2];
 
-            Console.WriteLine($"{product1}, cujo preço é $ {price1:F2}");
-            Console.WriteLine($"{product2}, cujo preço é $ {price2:F2}");
-            
-            Console.WriteLine();
+            Console.WriteLine(" ");
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
 
-            Console.WriteLine($"Registro: {age} anos de idade, código {code} e gênero: {gender}");
-
-            Console.WriteLine();
-
-            Console.WriteLine($"Medida com oito casas decimais: {measure:F8}");
-            Console.WriteLine($"Arredondando (três casas decimais): {measure.ToString("F3", CultureInfo.InvariantCulture)}");
+            Console.WriteLine(vet[0]);
+            Console.WriteLine(vet[1]);
+            Console.WriteLine(vet[2]);
         }
     }
 }
