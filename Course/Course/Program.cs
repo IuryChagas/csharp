cca7b2ddb078ab0a1843adea9c6a2c6bda813967
Iury Chagas <iurychagas@icloud.com>
2020-01-20 18:08:54 -0300

    add Criando primeira função em C# - MaiorNumber()

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -4,31 +4,30 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Digite seu nome completo: ");
-             string name = Console.ReadLine();
-            Console.WriteLine("Quantos quartos tem sua casa: ");
-             int bedroom = int.Parse(Console.ReadLine());
-            Console.WriteLine("Entre com o preço de um produto: ");
-             double product = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Digite seu último nome, idade e altura (tudo na mesma linha com espaços entre): ");
+            Console.WriteLine("Digite três números na mesma linha separados por um espaço: ");
             string[] vet = Console.ReadLine().Split(' ');
 
-            string nome = vet[0];
-            int idade = int.Parse(vet[1]);
-            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            int n1 = int.Parse(vet[0]);
+            int n2 = int.Parse(vet[1]);
+            int n3 = int.Parse(vet[2]);
 
-            Console.WriteLine(" ");
+            double resultado = MaiorNumero(n1, n2, n3);
+            Console.WriteLine("O Maior número é: "+ resultado);
+        }
 
-            Console.WriteLine(name);
-            Console.WriteLine(bedroom);
-            Console.WriteLine(product.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine(nome);
-            Console.WriteLine(idade.ToString(CultureInfo.InvariantCulture));
-            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        static int MaiorNumero(int a, int b, int c) {
+            int maiorDeTodos;
 
-
-
-
+            if (a > b && a > c) {
+                maiorDeTodos  = a;
+            }
+            else if (b > c) {
+                maiorDeTodos = b;
+            }
+            else {
+                maiorDeTodos = c;
+            }
+            return maiorDeTodos;
         }
     }
 }
