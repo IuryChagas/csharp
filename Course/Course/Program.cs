6465c1c705e603d23387f34757bfcd9be5f9ac16
Iury Chagas <iurychagas@icloud.com>
2020-01-20 17:33:30 -0300

    add Exercícios de fixação - output

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,34 +1,34 @@
 ﻿using System;
 using System.Globalization;
-
-
 namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            int n1 = int.Parse(Console.ReadLine());
-            char ch = char.Parse(Console.ReadLine());
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
+            Console.WriteLine("Digite seu nome completo: ");
+             string name = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem sua casa: ");
+             int bedroom = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+             double product = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite seu último nome, idade e altura (tudo na mesma linha com espaços entre): ");
             string[] vet = Console.ReadLine().Split(' ');
-            
-            string name = vet[0];
-            char gender = char.Parse(vet[1]);
-            int age = int.Parse(vet[2]);
-            double height = double.Parse(vet[3]);
 
-            Console.WriteLine("Você digitou: ");
-            
-            Console.WriteLine("");
-            Console.WriteLine(n1);
-            Console.WriteLine(ch);
-            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            string nome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
-            Console.WriteLine("");
+            Console.WriteLine(" ");
+
             Console.WriteLine(name);
-            Console.WriteLine(gender);
-            Console.WriteLine(age);
-            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(bedroom);
+            Console.WriteLine(product.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(idade.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
         }
     }
 }
