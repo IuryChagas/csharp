40b9321e8ee1d27a28da78e3a9e5afb72b1bba5f
Iury Chagas <iurychagas@icloud.com>
2020-01-21 15:43:12 -0300

    add Classe Funcionario - POO - Exercício 02

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,5 +1,5 @@
-﻿// Fazer um programa para ler os dados, nome e idade  de pessoas,
-// Mostrar os dados da pessoa mais velha.
+﻿// Fazer um programa para ler os dados, nome e salário de funcionários,
+// Mostrar o salário médio.
 
 using System;
 using System.Collections.Generic;
@@ -8,47 +8,30 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Pessoa primeiroUsuario, segundoUsuario;
+           Funcionario funcionario1, funcionario2;
 
-            primeiroUsuario = new Pessoa();
-            segundoUsuario = new Pessoa();
+            funcionario1 = new Funcionario();
+            funcionario2 = new Funcionario();
 
-            Console.Write("Digite o nome do(a) 1° usuário(a): ");
-            primeiroUsuario.Name = Console.ReadLine();
-            Console.Write("Digite a idade do(a) 1° usuário(a): ");
-            primeiroUsuario.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.Write("Digite o nome do(a) 2° usuário(a): ");
-            segundoUsuario.Name = Console.ReadLine();
-            Console.Write("Digite a idade do(a) 2° usuário(a): ");
-            segundoUsuario.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
-            if (primeiroUsuario.Age > segundoUsuario.Age) {
-                Console.WriteLine();
-                Console.WriteLine("### Cadastro com maior idade ###");
-                Console.WriteLine("");
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                Console.WriteLine($"Nome: {primeiroUsuario.Name}");
-                Console.WriteLine($"Idade: {primeiroUsuario.Age}");
-            }
-            else if (segundoUsuario.Age > primeiroUsuario.Age) {
-                Console.WriteLine();
-                Console.WriteLine("### Cadastro com maior idade ###");
-                Console.WriteLine("");
+            double mediaSalario = (funcionario1.Salario + funcionario2.Salario) / 2;
 
-                Console.WriteLine($"Nome: {segundoUsuario.Name}");
-                Console.WriteLine($"Idade: {segundoUsuario.Age}");
-            }
-            else if (segundoUsuario.Age == primeiroUsuario.Age) {
-                Console.WriteLine();
-                Console.WriteLine("### As pessoas cadastradas possuem mesma idade ###");
-                Console.WriteLine("");
-            }
-            else {
-                Console.WriteLine();
-                Console.WriteLine("### As pessoas cadastradas possuem mesma idade ###");
-                Console.WriteLine("");
-            }
+            Console.WriteLine("");
+            Console.WriteLine($" ### Salário médio: {mediaSalario.ToString("F2", CultureInfo.InvariantCulture)}  ###");
+            Console.WriteLine("");
         }
     }
 }
