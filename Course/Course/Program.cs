3114858897a1e90aeb8098272e21f30ad0905412
Iury Chagas <iurychagas@icloud.com>
2020-01-21 10:53:17 -0300

    add introdução à POO - script clássico

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -3,25 +3,52 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            int soma = 0;
+            double xA, xB, xC, yA, yB, yC;
 
-            Console.WriteLine("### DIGITE A QUANTIDADE DE NÚMEROS QUE QUER SOMAR ###");
-
-            Console.Write("Quantos números inteiros quer somar: ");
-            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            Console.Write("Digite o valor do lado xA: ");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado xB: ");
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado xC: ");
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine("");
 
-            for (int i = 1; i <= y; i++){
-                Console.Write(i +"° valor: ");
-                int numero = int.Parse(Console.ReadLine());
-                soma += numero;
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            Console.Write("Digite o valor do lado yA: ");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado yB: ");
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado yC: ");
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Área de X = "+ areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = "+ areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior Área: Triangulo X com " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             }
-            
-            Console.WriteLine("");
+            else if (areaY > areaX) {
+                Console.WriteLine("Maior Área: Triangulo Y com " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            }
+            else if (areaY == areaX) {
+                Console.WriteLine("");
 
-            Console.WriteLine("Soma total dos números digitados: "+ soma);
-            
+                Console.WriteLine("As Áreas dos triangulos são iguais!");
+                Console.WriteLine("");
+            }
+            else {
+                Console.WriteLine("\nOperação inválida... \nRepita todo processo!");
+            }
         }
     }
 }
