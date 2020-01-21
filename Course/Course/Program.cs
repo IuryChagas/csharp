e4221ab4c4bfb2e6a980e5b3fba6ec426775b77b
Iury Chagas <iurychagas@icloud.com>
2020-01-21 17:04:37 -0300

    add continuação de POO - Método que retorna Área

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,20 +1,56 @@
-﻿// Fazer um programa para ler os dados, nome e salário de funcionários,
-// Mostrar o salário médio.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Caneta canetaPremium = new Caneta();
-            canetaPremium.Modelo = "Mont Blanc";
-            canetaPremium.Cor = "Preta";
-            canetaPremium.Ponta = 0.5d;
-            canetaPremium.Carga = 100;
-            canetaPremium.Tampa = false;
-            canetaPremium.status();
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            Console.Write("Digite o valor do lado xA: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado xB: ");
+
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado xC: ");
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            Console.Write("Digite o valor do lado yA: ");
+
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado yB: ");
+
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o valor do lado yC: ");
+
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double areaX = x.Area();    
+            double areaY = y.Area();
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"Área de X: {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Área de Y: {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior área: X");
+            }
+            else {
+                Console.WriteLine("Maior área: Y");
+            }
+
+            Console.WriteLine("");
+
+
         }
     }
 }
