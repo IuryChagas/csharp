c2873844c0470067c23194301525ccd60492c19a
Iury Chagas <iurychagas@icloud.com>
2020-01-21 15:21:49 -0300

    add POO - exercício de revisão

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -4,38 +4,51 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
+            Pessoa primeiroUsuario, segundoUsuario;
 
-            List<Pessoa> pessoas = new List<Pessoa>();
+            primeiroUsuario = new Pessoa();
+            segundoUsuario = new Pessoa();
 
-            Console.Write("Quantas pessoas você quer cadastrar? ");
-            int numeroDeCadastros = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do(a) 1° usuário(a): ");
+            primeiroUsuario.Name = Console.ReadLine();
+            Console.Write("Digite a idade do(a) 1° usuário(a): ");
+            primeiroUsuario.Age = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < numeroDeCadastros; i++) {
-                Pessoa user = new Pessoa();
+            Console.Write("Digite o nome do(a) 2° usuário(a): ");
+            segundoUsuario.Name = Console.ReadLine();
+            Console.Write("Digite a idade do(a) 2° usuário(a): ");
+            segundoUsuario.Age = int.Parse(Console.ReadLine());
 
-                Console.WriteLine($"Digite os dados da {i+1}° pessoa: ");
-                Console.Write("Nome: ");
-                user.Name = Console.ReadLine();
-                Console.Write("Idade: ");
-                user.Age = int.Parse(Console.ReadLine());
+            if (primeiroUsuario.Age > segundoUsuario.Age) {
+                Console.WriteLine();
+                Console.WriteLine("### Cadastro com maior idade ###");
+                Console.WriteLine("");
 
-                pessoas.Add(user);
+                Console.WriteLine($"Nome: {primeiroUsuario.Name}");
+                Console.WriteLine($"Idade: {primeiroUsuario.Age}");
             }
+            else if (segundoUsuario.Age > primeiroUsuario.Age) {
+                Console.WriteLine();
+                Console.WriteLine("### Cadastro com maior idade ###");
+                Console.WriteLine("");
 
-            var pessoaMaisVelha = pessoas.OrderBy(p => p.Age).LastOrDefault();
-
-            Console.WriteLine("### Cadastro com maior idade ###");
-            Console.WriteLine("");
-
-            Console.WriteLine($"Nome: {pessoaMaisVelha.Name}");
-            Console.WriteLine($"Idade: {pessoaMaisVelha.Age}");
-
-
+                Console.WriteLine($"Nome: {segundoUsuario.Name}");
+                Console.WriteLine($"Idade: {segundoUsuario.Age}");
+            }
+            else if (segundoUsuario.Age == primeiroUsuario.Age) {
+                Console.WriteLine();
+                Console.WriteLine("### As pessoas cadastradas possuem mesma idade ###");
+                Console.WriteLine("");
+            }
+            else {
+                Console.WriteLine();
+                Console.WriteLine("### As pessoas cadastradas possuem mesma idade ###");
+                Console.WriteLine("");
+            }
         }
     }
 }
