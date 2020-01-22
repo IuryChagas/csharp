bb64bc80d58b5f02bd0ff295fb703ad0d71f2846
Iury Chagas <iurychagas@icloud.com>
2020-01-22 17:28:09 -0300

    add Membros estáticos = Parte 2

--- Triangulo/Triangulo/Program.cs
+++ Triangulo/Triangulo/Program.cs
@@ -3,29 +3,22 @@ using System.Globalization;
 
 namespace Triangulo {
     class Program {
-        static double Pi = 3.14;
         static void Main(string[] args) {
-           
+ 
             Console.Write("Entre com o valor de raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
+            Console.WriteLine("");
+            double circ = Triangulo.Circunferencia(raio);
             Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
 
-            double vol = Volume(raio);
+            double vol = Triangulo.Volume(raio);
             Console.WriteLine($"Volume: {vol.ToString("F2", CultureInfo.InvariantCulture)}");
 
-            Console.WriteLine($"Valor de PI: {Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor de PI: {Triangulo.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
         }
 
-        static double Circunferencia(double x) {
-
-            return 2.0 * Pi * x;
-        }
-        static double Volume(double x) {
-
-            return 4.0 / 3.0 * Pi * Math.Pow(x, 3.0);
-        }
+        
   
     }
 }
---
+++ Triangulo/Triangulo/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Triangulo {
+    class Triangulo {
+        public static double Pi = 3.14;
+        public static double Circunferencia(double x) {
+
+            return 2.0 * Pi * x;
+        }
+        public static double Volume(double x) {
+
+            return 4.0 / 3.0 * Pi * Math.Pow(x, 3.0);
+        }
+    }
+}
