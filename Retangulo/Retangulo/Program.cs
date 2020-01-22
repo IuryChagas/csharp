032d8dafaee2e20b0e0b913b22168afa7ca6889e
Iury Chagas <iurychagas@icloud.com>
2020-01-22 12:19:19 -0300

    add Script para calular Retangulo

---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("          ######### DIGITE A LARGURA E ALTURA DO RETANGULO #########");
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.Write("Largura: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine($"ÁREA: {retangulo.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO: {retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL: {retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Retangulo/Retangulo/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retangulo {
+    class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            double totalArea = Largura * Altura;
+            return totalArea;
+        }
+        public double Perimetro() {
+            double perimetro = 2 * (Largura + Altura);
+            return perimetro;
+        }
+        public double Diagonal() {
+            double diagonal = Math.Sqrt(Largura * Largura + Altura * Altura);
+            return diagonal;
+        }
+    }
+}
