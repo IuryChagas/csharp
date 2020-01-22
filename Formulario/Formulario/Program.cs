ce0111fc72f09e9f83609f5ff1b13656ffe117e6
Iury Chagas <iurychagas@icloud.com>
2020-01-22 13:34:24 -0300

    add POO - Formulário de novo Salário

---
+++ Formulario/Formulario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Formulario {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("         ### FORMULÁRIO PARA CADASTRAR FUNCIONÁRIO ###");
+
+            Funcionario funcionario = new Funcionario();
+            
+            Console.Write("NOME: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("SALÁRIO BRUTO: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("IMPOSTO: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Funcionário: {funcionario}");
+
+            Console.WriteLine("");
+            Console.Write($"Digite a porcentagem para aumentar o salário [00.0 à 100.0]: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Dados atualizados: {funcionario}");
+        }
+    }
+}
