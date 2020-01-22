beccb01778aebbb35c2f2980cde8e95554ae8baf
Iury Chagas <iurychagas@icloud.com>
2020-01-22 14:34:04 -0300

    add POO - Boletim escolar, - exercício 3

---
+++ Boletim/Boletim/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Boletim {
+    class Aluno {
+        public string Nome;
+        public double PrimeiraNota, SegundaNota, TerceiraNota;
+
+        public double MediaAnual() {
+            double media = PrimeiraNota + SegundaNota + TerceiraNota;
+
+            if (media >= 60.0) {
+                Console.WriteLine("APROVADO!");
+            }
+            else {
+                double faltam = 60.0 - media;
+                Console.WriteLine("REPROVADO! ");
+                Console.WriteLine($"FALTARAM {faltam.ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+
+            return media;
+        }
+    }
+}
---
+++ Boletim/Boletim/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Boletim {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("    #### BOLETIM ESCOLAR ####");
+            Console.WriteLine("");
+
+            Aluno aluno = new Aluno();
+            
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.Write("1° nota: ");
+            aluno.PrimeiraNota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("2° nota: ");
+            aluno.SegundaNota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("3° nota: ");
+            aluno.TerceiraNota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+
+
+            Console.WriteLine("NOTA FINAL = "+ aluno.MediaAnual().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
