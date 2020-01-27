7688d8173866a41142b82a038246f490e6dc3db8
Iury Chagas <iurychagas@icloud.com>
2020-01-27 15:07:21 -0300

    add  Intro Auto Properties

---
+++ Properties/Properties/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Properties {
+    class Produto {
+
+        private string _nome;
+        public double Preco {get ; private set; }
+        public int Quantidade { get; private set; }
+        
+
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+
+        }
+
+        public string Nome {
+            get { return _nome; }
+            set { if (value != null && value.Length > 1) {
+                    _nome = value;        
+                } 
+            }
+        }
+
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+
+        }
+        public double ValorTotalEmEstoque() {
+            double totalEmEstoque = (double) Preco * Quantidade;
+            return totalEmEstoque;
+        }
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() {
+            return _nome
+                + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Properties {
+    class Program {
+        static void Main(string[] args) {
+            Produto produto = new Produto("TV", 500.00, 10);
+            Console.WriteLine("Nome: " + produto.Nome);
+
+            produto.Nome = ("TV 4k");
+            Console.WriteLine("\n");
+
+            Console.WriteLine($"Novo nome: {produto.Nome}");
+            Console.WriteLine($"Preço: {produto.Preco}");
+            Console.WriteLine($"Quantidade: {produto.Quantidade}");
+        }
+    }
+}
