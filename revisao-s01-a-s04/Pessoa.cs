3e6ff7fc7087c8bc7ece74c90e898a0aa4b162a6
Iury Chagas <iurychagas@icloud.com>
2020-01-28 15:04:42 -0300

    add adicionando atributos - exer1

---
+++ revisao-s01-a-s04/Pessoa.cs
@@ -0,0 +1,8 @@
+namespace revisao_s01_a_s04
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ revisao-s01-a-s04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace revisao_s01_a_s04
+{
+    class Program
+    {
+        static void Main(string[] args){
+            
+            int maiorIdade = 0;
+            string nome = "";
+
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Ler os nomes e idades e retornar a maior idade: ");
+            Console.WriteLine("");
+            
+            for(int i = 1; i <= 3; i++){
+
+                System.Console.WriteLine("Dados da "+i+"° pessoa:");
+                System.Console.Write("Nome: ");
+                pessoa.Nome = Console.ReadLine();
+
+                System.Console.Write("Idade: ");
+                pessoa.Idade = int.Parse(Console.ReadLine());
+
+                System.Console.WriteLine("");
+
+                if(pessoa.Idade > maiorIdade){
+                    nome = pessoa.Nome;
+                    maiorIdade = pessoa.Idade;
+                }
+
+            }
+            System.Console.WriteLine("");
+            System.Console.WriteLine($"{nome} Tem a maior idade: {maiorIdade} anos");
+            System.Console.WriteLine("");
+        }
+    }
+}
