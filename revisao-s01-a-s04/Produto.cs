36cd629e80044a7583cbaf408ddf72044ce8e77c
Iury Chagas <iurychagas@icloud.com>
2020-01-29 09:44:50 -0300

    add método ToString / sobreposição

---
+++ revisao-s01-a-s04/Produto.cs
@@ -0,0 +1,30 @@
+using System.Globalization;
+
+namespace revisao_s01_a_s04 {
+    public class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque(){
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos( int quantidade) {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        override public string ToString(){
+            return Nome
+            +", Valor U$"
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: U$"
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- revisao-s01-a-s04/Program.cs
+++ revisao-s01-a-s04/Program.cs
@@ -1,39 +1,38 @@
 ﻿using System;
+using System.Globalization;
 
-namespace revisao_s01_a_s04
-{
-    class Program
-    {
+namespace revisao_s01_a_s04 {
+    class Program {
         static void Main(string[] args){
+            Produto produto = new Produto();
+
+            System.Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+
+            System.Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine($"Dados do Produto: {produto}");
+            System.Console.WriteLine();
+
+            System.Console.Write("Digite o n° de produtos à ser adicionados ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qtd);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine($"Dados atualizados: {produto}");
+
+            System.Console.WriteLine();
             
-            int maiorIdade = 0;
-            string nome = "";
+            System.Console.Write("Digite o n° de produtos à ser removidos do estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qtd);
 
-            Pessoa pessoa = new Pessoa();
-
-            Console.WriteLine("Ler os nomes e idades e retornar a maior idade: ");
-            Console.WriteLine("");
-            
-            for(int i = 1; i <= 3; i++){
-
-                System.Console.WriteLine("Dados da "+i+"° pessoa:");
-                System.Console.Write("Nome: ");
-                pessoa.Nome = Console.ReadLine();
-
-                System.Console.Write("Idade: ");
-                pessoa.Idade = int.Parse(Console.ReadLine());
-
-                System.Console.WriteLine("");
-
-                if(pessoa.Idade > maiorIdade){
-                    nome = pessoa.Nome;
-                    maiorIdade = pessoa.Idade;
-                }
-
-            }
-            System.Console.WriteLine("");
-            System.Console.WriteLine($"{nome} Tem a maior idade: {maiorIdade} anos");
-            System.Console.WriteLine("");
+            System.Console.WriteLine();
+            System.Console.WriteLine($"Dados atualizados: {produto}");
         }
     }
 }
