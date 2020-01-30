6a8b737c25f0738c7e7e49322e1141901f228912
Iury Chagas <iurychagas@icloud.com>
2020-01-30 10:55:30 -0300

    add intro encapsulamento - métodos get/set
    introdução e conceito do uso de encapsulamento

--- revisao-s01-a-s04/Program.cs
+++ revisao-s01-a-s04/Program.cs
@@ -5,37 +5,17 @@ namespace revisao_s01_a_s04 {
     class Program {
         static void Main(string[] args){
             
-            System.Console.Write("Nome: ");
-            string nome = Console.ReadLine();
+            Produto product = new Produto ("Notebook", 500.00, 10);
+                System.Console.WriteLine(product.GetQuantidade());
+                
+                System.Console.WriteLine(product.GetPreco().ToString("F2", CultureInfo.InvariantCulture));
+                
+                System.Console.Write("Setar um novo valor: ");
+                string nome = Console.ReadLine();
+                product.SetNome(nome);
+               
+                System.Console.Write("Valor do atributo _nome alterado para: "+product.GetNome());
 
-            System.Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            System.Console.Write("Quantidade no estoque: ");
-            int quantidade = int.Parse(Console.ReadLine());
-
-            Produto produto = new Produto(nome, preco, quantidade);
-
-            System.Console.WriteLine($"Dados do Produto: {produto}");
-            System.Console.WriteLine();
-
-
-            System.Console.Write("Digite o n° de produtos à ser adicionados ao estoque: ");
-            int qtd = int.Parse(Console.ReadLine());
-            produto.AdicionarProdutos(qtd);
-
-            System.Console.WriteLine();
-            System.Console.WriteLine($"Dados atualizados: {produto}");
-
-            System.Console.WriteLine();
-
-            System.Console.Write("Digite o n° de produtos à ser removidos do estoque: ");
-            qtd = int.Parse(Console.ReadLine());
-            produto.RemoverProdutos(qtd);
-
-            System.Console.WriteLine();
-            System.Console.WriteLine($"Dados atualizados: {produto}");
-
-        }            
+        }
     }
 }
