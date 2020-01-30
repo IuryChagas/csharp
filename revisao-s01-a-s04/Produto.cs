c57e3391a277564f979c73b15b4d382a046f4456
Iury Chagas <iurychagas@icloud.com>
2020-01-30 13:24:19 -0300

    add Properties

--- revisao-s01-a-s04/Produto.cs
+++ revisao-s01-a-s04/Produto.cs
@@ -6,50 +6,41 @@ namespace revisao_s01_a_s04 {
         private double _preco;
         private int _quantidade;
 
-        public Produto(){
-        }
+        public Produto () { }
 
-        public Produto (string nome, double preco, int quantidade){
+        public Produto (string nome, double preco, int quantidade) {
             _nome = nome;
             _preco = preco;
             _quantidade = quantidade;
         }
 
-        public int GetQuantidade (){
-            return _quantidade;
-        }
-        public string GetNome (){
-            return _nome;
+        public int Quantidade { 
+            get { return _quantidade; } 
+            set {}
         }
 
-        public void SetNome (string nome){
-            if(nome != null && nome.Length > 1){
-                _nome = nome;
+        public string Nome {
+            get { return _nome; }
+            set { 
+                if(value != null && value.Length > 1){
+                    _nome = value;
+                    System.Console.Write("Valor do atributo _nome alterado para: ");
+                } else {
+                    System.Console.Write("Valor do atributo _nome não alterado: ");
+                }
             }
-            
         }
-        public double GetPreco(){
-            return _preco;
+        public double Preco {
+            get { return _preco; }
         }
-        public double ValorTotalEmEstoque(){
+        public double ValorTotalEmEstoque () {
             return _quantidade * _preco;
         }
-
-        public void AdicionarProdutos( int quantidade) {
+        public void AdicionarProdutos (int quantidade) {
             _quantidade += quantidade;
         }
-        public void RemoverProdutos(int quantidade) {
+        public void RemoverProdutos (int quantidade) {
             _quantidade -= quantidade;
-        }
-
-        override public string ToString(){
-            return _nome
-            +", Valor U$"
-            + _preco.ToString("F2", CultureInfo.InvariantCulture)
-            + ", "
-            + _quantidade
-            + " unidades, Total: U$"
-            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
--- revisao-s01-a-s04/Program.cs
+++ revisao-s01-a-s04/Program.cs
@@ -6,16 +6,14 @@ namespace revisao_s01_a_s04 {
         static void Main(string[] args){
             
             Produto product = new Produto ("Notebook", 500.00, 10);
-                System.Console.WriteLine(product.GetQuantidade());
+                System.Console.WriteLine(product.Quantidade);
                 
-                System.Console.WriteLine(product.GetPreco().ToString("F2", CultureInfo.InvariantCulture));
+                System.Console.WriteLine(product.Preco.ToString("F2", CultureInfo.InvariantCulture));
                 
                 System.Console.Write("Setar um novo valor: ");
-                string nome = Console.ReadLine();
-                product.SetNome(nome);
-               
-                System.Console.Write("Valor do atributo _nome alterado para: "+product.GetNome());
+                product.Nome = Console.ReadLine();
 
+                System.Console.WriteLine("\n");
         }
     }
 }
