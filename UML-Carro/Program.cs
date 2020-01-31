340bd9a5c564c87f3242039b78fef7b576b187e5
Iury Chagas <iurychagas@icloud.com>
2020-01-31 09:19:21 -0300

    add starting projetct

---
+++ UML-Carro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UML_Carro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
